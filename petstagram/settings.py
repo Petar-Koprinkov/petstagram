120edecf2229d7188d04129bba6fab47c750ee1c
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-09-26 15:12:00 +0300

    feat: Added admin panel

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -82,12 +82,15 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "petstagram",
+        "USER": "postgres-user",
+        "PASSWORD": "56932957eH10",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
