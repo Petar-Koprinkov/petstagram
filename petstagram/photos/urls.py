92d8152cec2d6185db9d3bc0a5862509643c9fb7
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-10-08 12:52:50 +0300

    feat: Added hyperlinks to the main nav

--- petstagram/photos/urls.py
+++ petstagram/photos/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from petstagram.photos import views
 
 urlpatterns = [
-    path('add/', views.photo_add, name='add'),
+    path('add/', views.photo_add, name='add-photo'),
     path('<int:pk>/', include([
-        path('', views.photo_details, name='details'),
-        path('edit/', views.photo_edit, name='edit'),
+        path('', views.photo_details, name='details-photo'),
+        path('edit/', views.photo_edit, name='edit-photo'),
     ]))
 ]
