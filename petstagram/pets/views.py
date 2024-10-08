ddce32e77320d46aef17ca0431d968474dd6b3f5
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-10-08 17:58:50 +0300

    feat: Added functionality for pet details

--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render
 
+from petstagram.pets.models import Pet
+
+
 # Create your views here.
 
 
-def pet_add(request):
+def pet_add(request, username, slug):
     return render(request, 'pets/pet-add-page.html')
 
 
 def pet_details(request, username, slug):
-    return render(request, 'pets/pet-details-page.html')
+    pet = Pet.objects.get(slug=slug)
+    pet_photos = pet.pet_photos.all()
+
+    context = {
+        'pet': pet,
+        'pet_photos': pet_photos,
+    }
+    return render(request, 'pets/pet-details-page.html', context)
 
 
 def pet_edit(request, username, slug):
     return render(request, 'pets/pet-edit-page.html')
 
 
-def pet_delete(request):
+def pet_delete(request, username, slug):
     render(request, 'pets/pet-delete-page.html')
+
