286d09bd7c482c38886c7a4e4975ed2e0a998efc
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-10-09 13:33:52 +0300

    feat: added functionality to add pet in the application

--- petstagram/pets/forms.py
+++ petstagram/pets/forms.py
@@ -1,5 +1,35 @@
 from django import forms
 
+from petstagram.pets.models import Pet
 
-class PetForm(forms.ModelForm):
+
+class PetBaseForm(forms.ModelForm):
+    class Meta:
+        model = Pet
+        fields = ('name', 'date_of_birth', 'personal_photo')
+
+
+class PetAddForm(PetBaseForm):
+    class Meta:
+        model = Pet
+        fields = ('name', 'date_of_birth', 'personal_photo')
+
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Pet name'}),
+            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
+            'personal_photo': forms.TextInput(attrs={'placeholder': 'Link to image'})
+        }
+
+        labels = {
+            'name': 'Pet name',
+            'date_of_birth': 'Date of birth',
+            'personal_photo': 'Link to image'
+        }
+
+
+class PetEditForm(PetBaseForm):
+    pass
+
+
+class PetDeleteForm(PetBaseForm):
     pass
--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from petstagram.pets.forms import PetAddForm
 from petstagram.pets.models import Pet
 
 
-# Create your views here.
-
-
 def pet_add(request):
-    return render(request, 'pets/pet-add-page.html')
+    form = PetAddForm(request.POST or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('profile-details', pk=1)
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'pets/pet-add-page.html', context)
 
 
 def pet_details(request, username: str, slug: str):
@@ -27,4 +35,3 @@ def pet_edit(request, username: str, slug: str):
 
 def pet_delete(request, username: str, slug: str):
     render(request, 'pets/pet-delete-page.html')
-
