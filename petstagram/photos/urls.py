2c286e858e8fee90bad81f71c54c2f5758960a84
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-10-18 16:14:13 +0300

    feat: Migrated from FBV to CBV in photo application

--- petstagram/photos/urls.py
+++ petstagram/photos/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from petstagram.photos import views
 
 urlpatterns = [
-    path('add/', views.photo_add, name='add-photo'),
+    path('add/', views.AddPhotoView.as_view(), name='add-photo'),
     path('<int:pk>/', include([
-        path('', views.photo_details, name='details-photo'),
-        path('edit/', views.photo_edit, name='edit-photo'),
+        path('', views.PhotoDetailView.as_view(), name='details-photo'),
+        path('edit/', views.PhotoEditView.as_view(), name='edit-photo'),
         path('delete/', views.photo_delete, name='delete-photo'),
     ]))
 ]
--- petstagram/photos/views.py
+++ petstagram/photos/views.py
@@ -1,60 +1,35 @@
-from django.shortcuts import render, redirect
-
-from petstagram.common.forms import CommentForm
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, UpdateView
 from petstagram.photos.forms import PhotoAddForm, PhotoEditForm
 from petstagram.photos.models import Photo
 
 
-# Create your views here.
+class AddPhotoView(CreateView):
+    model = Photo
+    template_name = 'photos/photo-add-page.html'
+    form_class = PhotoAddForm
+    success_url = reverse_lazy('index')
 
 
-def photo_add(request):
-    form = PhotoAddForm(request.POST or None, request.FILES or None)
+class PhotoDetailView(DetailView):
+    model = Photo
+    template_name = 'photos/photo-details-page.html'
+    context_object_name = 'photo'
 
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            return redirect('index')
-
-    context = {
-        'form': form
-    }
-
-    return render(request, 'photos/photo-add-page.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = self.object.comment_set.all()
+        context['likes'] = self.object.like_set.all()
+        return context
 
 
-def photo_details(request, pk):
-    photo = Photo.objects.get(pk=pk)
-    likes = photo.like_set.all()
-    comments = photo.comment_set.all()
-
-    comment_form = CommentForm()
-
-    context = {
-        'photo': photo,
-        'likes': likes,
-        'comments': comments,
-        'comment_form': comment_form,
-    }
-
-    return render(request, 'photos/photo-details-page.html', context)
-
-
-def photo_edit(request, pk):
-    photo = Photo.objects.get(pk=pk)
-    form = PhotoEditForm(request.POST or None, instance=photo)
-
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            return redirect('index')
-
-    context = {
-        'form': form,
-        'photo': photo
-    }
-
-    return render(request, 'photos/photo-edit-page.html', context)
+class PhotoEditView(UpdateView):
+    model = Photo
+    template_name = 'photos/photo-edit-page.html'
+    form_class = PhotoEditForm
+    success_url = reverse_lazy('index')
+    context_object_name = 'photo'
 
 
 def photo_delete(request, pk):
