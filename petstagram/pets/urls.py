9ac63238e3d891304b9e975499a71d2bd30b0d80
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-10-18 15:56:59 +0300

    feat: Migrated from FBV to CBV in pet app

--- petstagram/pets/urls.py
+++ petstagram/pets/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from petstagram.pets import views
 
 urlpatterns = [
-    path('add/', views.pet_add, name='pet-add'),
+    path('add/', views.AddPetView.as_view(), name='pet-add'),
     path('<str:username>/pet/<slug:slug>/', include([
-        path('', views.pet_details, name='pet-details'),
-        path('edit/', views.pet_edit, name='pet-edit'),
-        path('delete/', views.pet_delete, name='pet-delete'),
+        path('', views.PetDetailView.as_view(), name='pet-details'),
+        path('edit/', views.EditPetView.as_view(), name='pet-edit'),
+        path('delete/', views.DeletePetView.as_view(), name='pet-delete'),
     ]))
 ]
--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,66 +1,54 @@
 from django.shortcuts import render, redirect
-
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from petstagram.common.forms import CommentForm
 from petstagram.pets.forms import PetAddForm, PetEditForm, PetDeleteForm
 from petstagram.pets.models import Pet
 
 
-def pet_add(request):
-    form = PetAddForm(request.POST or None)
+class AddPetView(CreateView):
+    model = Pet
+    form_class = PetAddForm
+    template_name = 'pets/pet-add-page.html'
 
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            return redirect('profile-details', pk=1)
-
-    context = {
-        'form': form
-    }
-
-    return render(request, 'pets/pet-add-page.html', context)
+    def get_success_url(self):
+        return reverse_lazy('profile-details', kwargs={'pk': 1})
 
 
-def pet_details(request, username: str, slug: str):
-    pet = Pet.objects.get(slug=slug)
-    pet_photos = pet.pet_photos.all()
-    comment_form = CommentForm()
+class PetDetailView(DetailView):
+    model = Pet
+    context_object_name = 'pet'
+    template_name = 'pets/pet-details-page.html'
 
-    context = {
-        'pet': pet,
-        'pet_photos': pet_photos,
-        'comment_form': comment_form,
-    }
-    return render(request, 'pets/pet-details-page.html', context)
-
-
-def pet_edit(request, username: str, slug: str):
-    pet = Pet.objects.get(slug=slug)
-    form = PetEditForm(request.POST or None, instance=pet or None)
-
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            return redirect('pet-details', username, slug)
-
-    context = {
-        'pet': pet,
-        'form': form
-    }
-
-    return render(request, 'pets/pet-edit-page.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        context['pet_photos'] = self.object.pet_photos.all()
+        return context
 
 
-def pet_delete(request, username: str, slug: str):
-    pet = Pet.objects.get(slug=slug)
-    form = PetDeleteForm(instance=pet)
+class EditPetView(UpdateView):
+    model = Pet
+    form_class = PetEditForm
+    template_name = 'pets/pet-edit-page.html'
 
-    if request.method == 'POST':
-        pet.delete()
-        return redirect('profile-details', pk=1)
+    def get_success_url(self):
+        return reverse_lazy('pet-details', kwargs={'username': self.kwargs['username'], 'slug': self.kwargs['slug']})
 
-    context = {
-        'pet': pet,
-        'form': form,
-    }
 
-    return render(request, 'pets/pet-delete-page.html', context)
+class DeletePetView(DeleteView):
+    model = Pet
+    template_name = 'pets/pet-delete-page.html'
+    form_class = PetDeleteForm
+    success_url = reverse_lazy('profile-details', kwargs={'pk': 1})
+
+    def get_initial(self):
+        pet = self.get_object()
+        return pet.__dict__
+
+    def get_form_kwargs(self):
+        kwargs = {
+            'data': self.get_initial(),
+        }
+
+        return kwargs
