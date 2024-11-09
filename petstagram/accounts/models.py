272d5a13de2a5e13b93dae1bd50046aa346f7e91
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-11-09 21:52:44 +0200

    update: Extending the Abstract Base User Model

--- petstagram/accounts/models.py
+++ petstagram/accounts/models.py
@@ -1,3 +1,68 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, AbstractUser
 from django.db import models
+from petstagram.accounts.managers import CustomUserManager
 
-# Create your models here.
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(
+        unique=True,
+        help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
+        error_messages={
+            "unique": "A user with that email already exists.",
+        },
+    )
+
+    is_staff = models.BooleanField(
+        "staff status",
+        default=False,
+        help_text="Designates whether the user can log into this admin site.",
+    )
+    is_active = models.BooleanField(
+        "active",
+        default=True,
+        help_text=(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        to=CustomUser,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    first_name = models.CharField(
+        max_length=30,
+        blank=True,
+        null=True,
+    )
+
+    last_name = models.CharField(
+        max_length=30,
+        blank=True,
+        null=True,
+    )
+
+    date_of_birth = models.DateField(
+        blank=True,
+        null=True,
+    )
+
+    profile_picture = models.URLField(
+        blank=True,
+        null=True,
+    )
+
