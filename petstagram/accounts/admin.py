1c002ed79f7c4600c55dc9da5ea79510cdd81171
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-11-09 22:18:07 +0200

    Update: Modified UserAdmin

--- petstagram/accounts/admin.py
+++ petstagram/accounts/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from petstagram.accounts.forms import AppUserChangeForm, AppUserCreationForm
 
-# Register your models here.
+UserModel = get_user_model()
+
+
+@admin.register(UserModel)
+class CustomUserAdmin(UserAdmin):
+    model = UserModel
+    form = AppUserChangeForm
+    add_form = AppUserCreationForm
+
+    list_display = ("pk", "email", "is_staff", "is_superuser")
+    ordering = ("pk",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ()}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        ("Important dates", {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email",  "password1", "password2"),
+            },
+        ),
+    )
